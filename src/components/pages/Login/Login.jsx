02eda65c54307dab1d229a4c41a8fc5791a7f060
Lopezzz099 <nacholopez1903@gmail.com>
2023-04-25 19:43:52 -0300

    Poniendo los inputs del login

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,10 +1,26 @@
+// import { Link } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
   return (
-    <div>
-        <h1>Este es el login</h1>
+    <div className="login">
+      <button className="volverHome">Go Back</button>
+      <form className="cajaLogin">
+        <h1>Login</h1>
+        <div className="inputsLogin">
+          <div className="cajaEmailLogin">
+            <label className="loginEmail">Email Address</label>
+            <input type="email" className="emailLogin" />
+          </div>
+          <div className="cajaPassLogin">
+            <label className="loginPassword">Password</label>
+            <input type="password" className="passLogin" />
+          </div>
+        </div>
+        <button></button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
