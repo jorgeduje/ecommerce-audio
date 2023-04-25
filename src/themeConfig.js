9cd63539fadb8bf9a79d69efd01cde56adb3a5c0
AlfonsoG-M <alfonsogarciamansilla@gmail.com>
2023-04-25 16:34:34 -0300

    Creando contexto de colores - ThemeProvider

---
+++ src/themeConfig.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+export const theme = createTheme({
+    palette:{
+        primary:{
+            main: "#D87D4A",
+            second:"#101010",
+            third:"#F1F1F1",
+            fourth:"#FAFAFA"
+        },
+        secondary:{
+            main:"#FBAF85",
+            second: "FFFFFF",
+            third: "#000000"
+        }
+    }
+});
