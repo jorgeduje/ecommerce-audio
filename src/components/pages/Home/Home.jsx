4a5ccee029797f8b5f09655c1e540ec3d25929c0
AlfonsoG-M <alfonsogarciamansilla@gmail.com>
2023-04-26 09:40:15 -0300

    Actualizando home

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,10 +1,27 @@
-
+import { Typography, Button, Grid, Box } from "@mui/material";
 const Home = () => {
   return (
     <div>
-        <h1>Este es el home</h1>
+      <Grid container bgcolor={"primary.casa"} sx={{}}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="subtitle1" color="primary.third">
+            NEW PRODUCT
+          </Typography>
+          <Typography variant="h1" color="primary.fourth">
+            XX99 Mark II Headphones
+          </Typography>
+          <Typography paragraph={true} color="primary.third">
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </Typography>
+          <Button variant="contained"sx={{borderRadius:"0px"}}>SEE PRODUCT</Button>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <img src="https://res.cloudinary.com/dkwjizhdg/image/upload/v1682512392/e-commerce/headsphones_fqjywk.jpg" alt="headphones" />
+        </Grid>
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/themeConfig.js
+++ src/themeConfig.js
@@ -3,7 +3,7 @@ export const theme = createTheme({
     palette:{
         primary:{
             main: "#D87D4A",
-            second:"#101010",
+            casa:"#101010",
             third:"#F1F1F1",
             fourth:"#FAFAFA"
         },
