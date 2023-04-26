182c4890f958ee9a5c839f44c834db699480e806
jorgeduje <jorgeduje4@gmail.com>
2023-04-26 09:59:04 -0300

    se cambia h1 por h2 para que no rompa el width

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
           <Typography variant="subtitle1" color="primary.third">
             NEW PRODUCT
           </Typography>
-          <Typography variant="h1" color="primary.fourth">
+          <Typography variant="h2" color="primary.fourth">
             XX99 Mark II Headphones
           </Typography>
           <Typography paragraph={true} color="primary.third">
@@ -17,7 +17,7 @@ const Home = () => {
           <Button variant="contained"sx={{borderRadius:"0px"}}>SEE PRODUCT</Button>
         </Grid>
         <Grid item xs={12} md={6}>
-          <img src="https://res.cloudinary.com/dkwjizhdg/image/upload/v1682512392/e-commerce/headsphones_fqjywk.jpg" alt="headphones" />
+          <img style={{width: "100%"}} src="https://res.cloudinary.com/dkwjizhdg/image/upload/v1682512392/e-commerce/headsphones_fqjywk.jpg" alt="headphones" />
         </Grid>
         
       </Grid>
