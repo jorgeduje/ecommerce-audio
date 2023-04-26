959311cb40887c293324723ad794e35fa39c9d4e
Lopezzz099 <nacholopez1903@gmail.com>
2023-04-26 14:12:13 -0300

    Probando mui en login

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,25 +1,68 @@
 // import { Link } from "react-router-dom";
 import "./Login.css";
+import { Box, Button } from "@mui/material";
 
 const Login = () => {
   return (
-    <div className="login">
-      <button className="volverHome">Go Back</button>
-      <form className="cajaLogin">
-        <h1>Login</h1>
-        <div className="inputsLogin">
-          <div className="cajaEmailLogin">
-            <label className="loginEmail">Email Address</label>
-            <input type="email" className="emailLogin" placeholder="alexei@mail.com"/>
+    <Box
+      sx={{
+        minHeight: "85vh",
+        padding: "100px 200px",
+        backgroundColor: "primary.third",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Box sx={{ width: "50%" }}>
+        <Button
+          className="volverHome"
+          sx={{
+            textDecoration: "none",
+            color: "secondary.third",
+            opacity: "0.5",
+            marginBottom: "28px",
+            backgroundColor: "primary.third",
+            outline: "0",
+            textAlign: "start",
+            border: "0",
+            padding: "0",
+            display: "flex",
+            justifyContent: "flex-start",
+            cursor: "pointer",
+            transition: "none",
+            webkitTransition: "none",
+            textTransform: "none",
+          }}
+        >
+          Go Back
+        </Button>
+      </Box>
+      <Box>
+        <form className="cajaLogin">
+          <h1>Login</h1>
+          <div className="inputsLogin">
+            <div className="cajaEmailLogin">
+              <label className="loginEmail">Email Address</label>
+              <input
+                type="email"
+                className="emailLogin"
+                placeholder="alexei@mail.com"
+              />
+            </div>
+            <div className="cajaPassLogin">
+              <label className="loginPassword">Password</label>
+              <input
+                type="password"
+                className="passLogin"
+                placeholder="12345"
+              />
+            </div>
           </div>
-          <div className="cajaPassLogin">
-            <label className="loginPassword">Password</label>
-            <input type="password" className="passLogin" placeholder="12345"/>
-          </div>
-        </div>
-        <button className="botonLogin">Continue</button>
-      </form>
-    </div>
+          <button className="botonLogin">Continue</button>
+        </form>
+      </Box>
+    </Box>
   );
 };
 
