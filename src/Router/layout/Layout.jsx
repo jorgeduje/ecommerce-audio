79055be41599b3e96fa8f09b41cd86d854b6197b
jorgeduje <jorgeduje4@gmail.com>
2023-04-26 14:17:34 -0300

    se corrigen rutas de importacion

--- src/Router/layout/Layout.jsx
+++ src/Router/layout/Layout.jsx
@@ -1,6 +1,7 @@
-import Footer from "../../components/footer/Footer"
+
 import { Outlet } from "react-router-dom";
-import HeaderContainer from "../../components/header/HeaderContainer";
+import Footer from "../../components/common/footer/Footer";
+import HeaderContainer from "../../components/common/header/HeaderContainer";
 
 const Layout = () => {
   return (
