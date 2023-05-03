a038e9c48ce209f863461ccfb8a3a77975e9f126
monimunozalzate <106264010+monimunozalzate@users.noreply.github.com>
2023-05-03 11:47:05 -0500

    removiendo titulo 'productos'

--- src/components/pages/ProductsList/ProductsList.jsx
+++ src/components/pages/ProductsList/ProductsList.jsx
@@ -4,7 +4,6 @@ import ProductCardContainer from "../../common/productCard/ProductCardContainer"
 const ProductsList = ({ items }) => {
   return (
     <div className={styles.categoryContainer}>
-      <h1>Productos</h1>
       {items.map((product) => (
         <ProductCardContainer product={product} key={product.id}/>
       ))}
