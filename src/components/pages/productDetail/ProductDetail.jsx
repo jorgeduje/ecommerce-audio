43648578604ecb5470a998dd2bfa862c5c79eaed
jorgeduje <jorgeduje4@gmail.com>
2023-05-04 14:32:26 -0300

    se corrige bug de productDetail

--- src/components/pages/productDetail/ProductDetail.jsx
+++ src/components/pages/productDetail/ProductDetail.jsx
@@ -1,24 +1,36 @@
-import { Box, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material"
-import ProductCardContainer from "../../common/productCard/ProductCardContainer"
+import { Box, Typography } from "@mui/material";
+import ProductCardContainer from "../../common/productCard/ProductCardContainer";
 
-const ProductDetail = ({product}) => {
-
-    const {includes} =  product
-    console.log(includes);
-    return (
-        <Box sx={{ dispplay: "flex", padding: "165px" }}>
-            <ProductCardContainer product={product}/>
-            <Box component={"section"} sx={{ display: "flex", justifyContent: "space-between", marginTop: "83px" }}>
-                <Box>
-                <Typography variant="h2" sx={{ width: "50%" }}>FEATURES</Typography>
-                <Typography variant="p">{product.features}</Typography>
-                </Box>
-                <Box>
-                <Typography variant="h2">IN THE BOX</Typography>
-                {includes || includes.map((item, index) => <Typography key={index}>{Object.keys(item)}</Typography>)}
-                </Box>
-            </Box>
+const ProductDetail = ({ product }) => {
+  return (
+    <Box sx={{ dispplay: "flex", padding: "165px" }}>
+      
+        <ProductCardContainer product={product} />
+      
+      {/* <ProductCardContainer product={product} /> */}
+      <Box
+        component={"section"}
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: "83px",
+        }}
+      >
+        <Box>
+          <Typography variant="h2" sx={{ width: "50%" }}>
+            FEATURES
+          </Typography>
+          <Typography variant="p">{product.features}</Typography>
         </Box>
-    )
-}
-export default ProductDetail
+        <Box>
+          <Typography variant="h2">IN THE BOX</Typography>
+          {
+            product.includes.map((item, index) => (
+              <Typography key={index}>{item.item}</Typography>
+            ))}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+export default ProductDetail;
--- src/components/pages/productDetail/ProductDetailContainer.jsx
+++ src/components/pages/productDetail/ProductDetailContainer.jsx
@@ -1,27 +1,36 @@
 // import React from 'react'
-import { useEffect, useState } from 'react';
-import ProductDetail from './ProductDetail'
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import ProductDetail from "./ProductDetail";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Loading from "../../common/loading/Loading";
 
 const ProductDetailContainer = () => {
+  const [product, setProduct] = useState(null);
+  const { id } = useParams();
 
-    const [product, setproduct] = useState('')
-    const {id} = useParams()
-    
-    useEffect(() => {
-        axios.get(`http://localhost:5000/products/${id}`)
-        .then(res =>{
-            setproduct(res.data)
-        })
-        .catch(error => {
-            console.error(error)
-        })
-      }, [id]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/products/${id}`)
+      .then((res) => {
+        setTimeout(() => {
+          setProduct(res.data);
+        }, 2000);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [id]);
+
+  // if(!product){
+  //   return <h1>Cargando......</h1>
+  // }
 
   return (
-    <ProductDetail product={product}/>
-  )
-}
+    <div>
+      { !product ? <Loading /> : <ProductDetail product={product} />}
+    </div>
+  );
+};
 
-export default ProductDetailContainer
+export default ProductDetailContainer;
