e2f54d016e3593cd9999fe90edb09de54b69b354
jorgeduje <jorgeduje4@gmail.com>
2023-05-26 14:40:35 -0300

    se corrige checkout

--- src/components/pages/Checkout/CheckoutContainer.jsx
+++ src/components/pages/Checkout/CheckoutContainer.jsx
@@ -1,53 +1,83 @@
-import Checkout from "./Checkout"
+import Checkout from "./Checkout";
 import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import CheckoutModalContainer from "../../common/checkoutModal/CheckoutModalContainer";
+import { useState } from "react";
+import { clearCart } from "../../../store/cart/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const CheckoutContainer = () => {
-    const { handleSubmit, handleChange, errors, values } = useFormik({
-        initialValues: {
-          name: "",  
-          email: "",
-          phone: "",
-          address: "",
-          zipCode: "",
-          city: "",
-          country: "",
-          paymentMethod: "",
-          eMoneyNumber: "",
-          eMoneyPin: ""
-        },
-        onSubmit: (data) => {
-          console.log(data);
-        },
-        validationSchema: Yup.object().shape({
-          name:   Yup.string().required("Este es campo es obligatorio"),
-          email: Yup.string()
-            .email("El texto no corresponde a un email")
-            .required("Este es campo es obligatorio"),
-          phone: Yup.number().required("Este es campo es obligatorio"),
-          address: Yup.string().required("Este es campo es obligatorio"),
-          zipCode: Yup.string().required("Este es campo es obligatorio"),
-          city: Yup.string().required("Este es campo es obligatorio"),
-          country: Yup.string().required("Este es campo es obligatorio"),
-          paymentMethod: Yup.string().required("Este es campo es obligatorio"),
-          eMoneyNumber: Yup.number().required("Este es campo es obligatorio"),
-          eMoneyPin: Yup.number().required("Este es campo es obligatorio"),
-        }),
-        validateOnChange: false,
-      });
-    
-      const navigate = useNavigate();
-    
-      return (
-        <Checkout
-          navigate={navigate}
-          handleChange={handleChange}
-          handleSubmit={handleSubmit}
-          errors={errors}
-          values={values}
-        />
-      );
-}
+  const { cart, total } = useSelector((store) => store.cartSlice);
+  let shipping = 50;
+  let vat = total * 0.21;
+  let grandTotal = total + vat + shipping;
+  const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    dispatch(clearCart());
+  };
 
-export default CheckoutContainer
+  const { handleSubmit, handleChange, errors, values } = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      phone: "",
+      address: "",
+      zipCode: "",
+      city: "",
+      country: "",
+      paymentMethod: "",
+      eMoneyNumber: "",
+      eMoneyPin: "",
+    },
+    onSubmit: (data) => {
+      console.log(data);
+      handleOpen();
+    },
+    validationSchema: Yup.object().shape({
+      name: Yup.string().required("Este es campo es obligatorio"),
+      email: Yup.string()
+        .email("El texto no corresponde a un email")
+        .required("Este es campo es obligatorio"),
+      phone: Yup.number().required("Este es campo es obligatorio"),
+      address: Yup.string().required("Este es campo es obligatorio"),
+      zipCode: Yup.string().required("Este es campo es obligatorio"),
+      city: Yup.string().required("Este es campo es obligatorio"),
+      country: Yup.string().required("Este es campo es obligatorio"),
+      paymentMethod: Yup.string().required("Este es campo es obligatorio"),
+      eMoneyNumber: Yup.number().required("Este es campo es obligatorio"),
+      eMoneyPin: Yup.number().required("Este es campo es obligatorio"),
+    }),
+    validateOnChange: false,
+  });
+
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Checkout
+        navigate={navigate}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        errors={errors}
+        values={values}
+        cart={cart}
+        grandTotal={grandTotal}
+        shipping={shipping}
+        vat={vat}
+        total={total}
+      />
+      <CheckoutModalContainer
+        open={open}
+        handleOpen={handleOpen}
+        handleClose={handleClose}
+        grandTotal={grandTotal}
+      />
+    </>
+  );
+};
+
+export default CheckoutContainer;
